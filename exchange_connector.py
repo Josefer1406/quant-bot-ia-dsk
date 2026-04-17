9323e0bc0c5a892e47961c230ff890bcf7e611f1
Josefer1406 <miler126@gmail.com>
2026-04-17 14:49:07 -0500

    Update exchange_connector.py

--- exchange_connector.py
+++ exchange_connector.py
@@ -3,32 +3,12 @@ import config
 
 class ExchangeConnector:
     def __init__(self):
-        if config.SIMULATION_MODE:
-            if config.EXCHANGE_NAME == "okx":
-                self.exchange = ccxt.okx({'enableRateLimit': True})
-            elif config.EXCHANGE_NAME == "binance":
-                self.exchange = ccxt.binance({'enableRateLimit': True})
+        if config.EXCHANGE_NAME == "bybit":
+            self.exchange = ccxt.bybit({'enableRateLimit': True})
         else:
-            if config.EXCHANGE_NAME == "okx":
-                self.exchange = ccxt.okx({
-                    'apiKey': config.OKX_API_KEY,
-                    'secret': config.OKX_SECRET_KEY,
-                    'password': config.OKX_PASSPHRASE,
-                    'enableRateLimit': True,
-                })
-            elif config.EXCHANGE_NAME == "binance":
-                self.exchange = ccxt.binance({
-                    'apiKey': config.BINANCE_API_KEY,
-                    'secret': config.BINANCE_SECRET_KEY,
-                    'enableRateLimit': True,
-                })
-            else:
-                raise ValueError("Exchange desconocido")
+            self.exchange = ccxt.bybit({'enableRateLimit': True})
     
     def fetch_ohlcv(self, symbol, timeframe, limit):
         return self.exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
-    
-    def fetch_ticker(self, symbol):
-        return self.exchange.fetch_ticker(symbol)
 
 exchange = ExchangeConnector()

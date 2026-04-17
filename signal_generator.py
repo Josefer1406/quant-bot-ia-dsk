5724e718c231b01594956562c9e0966642804a11
Josefer1406 <miler126@gmail.com>
2026-04-16 20:18:27 -0500

    Update signal_generator.py

--- signal_generator.py
+++ signal_generator.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import numpy as np
 from features import add_technical_features
 from regime_detector import detect_market_regime, get_regime_multiplier
 from ml_model import ml_model
@@ -9,10 +7,12 @@ def generate_signal(symbol, df):
     if df is None or len(df) < 100:
         return None, 0.0, 0.0
     df_feat = add_technical_features(df)
+    if df_feat is None or df_feat.empty:
+        return None, 0.0, 0.0
     last = df_feat.iloc[-1]
-    trend_bull = last['ema_21'] > last['ema_50'] > last['ema_200']
-    price_above_ema = last['close'] > last['ema_21']
-    momentum_positive = last['returns_5'] > 0
+    trend_bull = last.get('ema_21', 0) > last.get('ema_50', 0) > last.get('ema_200', 0)
+    price_above_ema = last.get('close', 0) > last.get('ema_21', 0)
+    momentum_positive = last.get('returns_5', 0) > 0
     tech_score = (trend_bull * 0.4) + (price_above_ema * 0.3) + (momentum_positive * 0.3)
     ml_prob = ml_model.predict_probability(df)
     if ml_prob is None:
